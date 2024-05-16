0b271607a5dde6042059c0c5a9ace02386047bcf
MohamedRamdan1 <medo44448888@gmail.com>
2024-05-16 10:14:30 +0300

    refactor: Update learnings page with new components and sections

--- app/[locale]/learnings/page.tsx
+++ app/[locale]/learnings/page.tsx
@@ -1,9 +1,19 @@
+import Contact from '@/components/Contact'
+import FAQsSection from '@/components/FAQsSection'
+import Newsletter from '@/components/Newsletter'
 import Home from '@/components/learnings/home'
+import Statistics from '@/components/learnings/statistics'
+import WhyUs from '@/components/learnings/whyUs'
 
 export default function Page() {
   return (
     <div>
       <Home />
+      <Statistics />
+      <WhyUs />
+      <FAQsSection name="OS.FAQs" />
+      <Contact />
+      <Newsletter />
     </div>
   )
 }
