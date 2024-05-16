73ed30998be40e4e1bee038cdd44ec8118d51294
MohamedRamdan1 <medo44448888@gmail.com>
2024-05-16 10:11:30 +0300

    feat: Add WhyUs component for displaying learning features

---
+++ components/learnings/whyUs.tsx
@@ -0,0 +1,24 @@
+import { Airdrop, Moneys, People, Profile, Rank, Timer } from 'iconsax-react'
+import Feature from '../Feature'
+import Section from '../Section'
+
+export default function WhyUs() {
+  return (
+    <Section name="learnings.whyUs">
+      <div className="grid grid-cols-1 md:gap-x-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        <Feature feature="learnings.whyUs.1">
+          <Rank variant="Bulk" />
+        </Feature>
+        <Feature feature="learnings.whyUs.2">
+          <People variant="Bulk" />
+        </Feature>
+        <Feature feature="learnings.whyUs.3">
+          <Moneys variant="Bulk" />
+        </Feature>
+        <Feature feature="learnings.whyUs.4">
+          <Airdrop variant="Bulk" />
+        </Feature>
+      </div>
+    </Section>
+  )
+}
