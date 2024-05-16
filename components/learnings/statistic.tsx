a9950f0f193e879ca0d60f5bdd6ce24541ecd6de
MohamedRamdan1 <medo44448888@gmail.com>
2024-05-16 06:49:29 +0300

    chore: Add Statistic component for displaying learning statistics

---
+++ components/learnings/statistic.tsx
@@ -0,0 +1,16 @@
+'use client'
+import { useTranslations } from 'next-intl'
+
+interface StatisticProps {
+  name: string
+}
+export default function Statistic({ name }: StatisticProps) {
+  const t = useTranslations(name)
+  return (
+    <p className="text-main-dark font-medium text-xl">
+      <span className="text-main-color font-bold text-3xl">{t('number')}</span>
+      <br />
+      {t('text')}
+    </p>
+  )
+}
