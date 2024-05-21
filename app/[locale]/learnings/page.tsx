54103db04bdf06e454f12d0acfdafcd4d178bc3b
MohamedRamdan1 <medo44448888@gmail.com>
2024-05-21 15:04:23 +0300

    refactor: Add FeaturedCourses component to learnings page

--- app/[locale]/learnings/page.tsx
+++ app/[locale]/learnings/page.tsx
@@ -1,16 +1,26 @@
 import Contact from '@/components/Contact'
 import FAQsSection from '@/components/FAQsSection'
 import Newsletter from '@/components/Newsletter'
+import FeaturedCourse from '@/components/learnings/featuredCourse'
+import FeaturedCourses from '@/components/learnings/featuredCourses'
 import Home from '@/components/learnings/home'
 import Statistics from '@/components/learnings/statistics'
 import WhyUs from '@/components/learnings/whyUs'
 
-export default function Page() {
+export default async function Page() {
+  const getCourses = async () => {
+    const res = await fetch('https://new.athr-ksa.com/api/all-courses')
+    const data = await res.json()
+    return data.data
+  }
+  const courses = await getCourses()
+  console.log('getCourses', await getCourses())
   return (
     <div>
       <Home />
       <Statistics />
       <WhyUs />
+      <FeaturedCourses courses={courses} />
       <FAQsSection name="OS.FAQs" />
       <Contact />
       <Newsletter />
