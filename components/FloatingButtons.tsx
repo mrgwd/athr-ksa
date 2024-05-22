71ee36ea99498b662351c0da59d4bf4447cc9a30
MohamedRamdan1 <medo44448888@gmail.com>
2024-05-22 12:54:20 +0300

    refactor: Update FloatingButtons component styles for responsive design and better UX

--- components/FloatingButtons.tsx
+++ components/FloatingButtons.tsx
@@ -2,7 +2,7 @@ import { CallCalling, Whatsapp } from 'iconsax-react'
 
 export default function FloatingContactButton() {
   return (
-    <div className="float fixed bottom-4 right-4 z-20 sm:bottom-12 sm:right-12">
+    <div className="float fixed bottom-24 right-4 z-20 sm:bottom-12 sm:right-12">
       <div className="overflow-hidden p-2 transition duration-300 child:mb-2 child:block child:rounded-full child:p-4 child:child:text-white child:transition-all child-hover:scale-105">
         <a href="tel:+966506354147" target="_blank" className="bg-gray-800">
           <CallCalling variant="Bold" />
