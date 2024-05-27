005a85a909831b16c3f746b0d2d87087bbb86561
MohamedRamdan1 <medo44448888@gmail.com>
2024-05-27 06:54:53 +0300

    refactor: Update metadata to be dynamically generated based on the language using generateMetadata

--- app/[locale]/blog/page.tsx
+++ app/[locale]/blog/page.tsx
@@ -1,9 +1,7 @@
 import { Metadata } from 'next'
 import Articles from '@/components/Articles'
-import { Article } from '@/types/articlesTypes'
-import { title } from 'process'
-export const metadata: Metadata = {
-  title: 'المدونة - أثر',
+const arabicMetadata: Metadata = {
+  title: 'المدونة: بوابتُك لِعالم التقنية',
   keywords: [
     'مؤسسة اثر للتسويق الرقمي',
     'مدونة أثر',
@@ -28,6 +26,55 @@ export const metadata: Metadata = {
   ],
   description:
     'في مدونة أثر، نشاركك أحدث الأفكار والمقالات حول التسويق الرقمي، تصميم وبرمجة المواقع، تصميم التطبيقات، والكثير من المواضيع الأخرى المتعلقة بالتكنولوجيا والتسويق الرقمي.',
+  alternates: {
+    canonical: 'https://athr-ksa.com/ar/blog',
+    languages: {
+      en: 'https://athr-ksa.com/en/blog',
+      'ar-SA': 'https://athr-ksa.com/ar/blog',
+    },
+  },
+}
+
+const englishMetadata: Metadata = {
+  title: 'Blog: Tech, Design, and Marketing Insights',
+  keywords: [
+    'Athar digital marketing agency',
+    'Athar blog',
+    'web design',
+    'SEO',
+    'UI/UX design',
+    'app design',
+    'web development',
+    'e-commerce',
+    'app development',
+    'custom development',
+    'digital marketing',
+    'advertising',
+    'SEO optimization',
+    'marketing content',
+    'advertising content',
+    'exclusive content',
+    'motion graphics',
+    'branding',
+    'accounting software',
+    'accounting systems',
+  ],
+  description:
+    'In the ATHR blog, we share with you the latest ideas and articles about digital marketing, web design and development, app design, and many other topics related to technology and digital marketing.',
+  alternates: {
+    canonical: 'https://athr-ksa.com/en/blog',
+    languages: {
+      en: 'https://athr-ksa.com/en/blog',
+      'ar-SA': 'https://athr-ksa.com/ar/blog',
+    },
+  },
+}
+export async function generateMetadata({
+  params,
+}: {
+  params: { locale: string }
+}): Promise<Metadata> {
+  return params.locale === 'ar' ? arabicMetadata : englishMetadata
 }
 export default async function Page() {
   const getUserIp = async () => {
--- app/[locale]/learnings/page.tsx
+++ app/[locale]/learnings/page.tsx
@@ -1,15 +1,68 @@
 import Contact from '@/components/Contact'
 import FAQsSection from '@/components/FAQsSection'
 import Newsletter from '@/components/Newsletter'
-import FeaturedCourse from '@/components/learnings/featuredCourse'
 import FeaturedCourses from '@/components/learnings/featuredCourses'
 import Home from '@/components/learnings/home'
+import ReviewsSection from '@/components/learnings/reviewsSection'
 import Statistics from '@/components/learnings/statistics'
 import WhyUs from '@/components/learnings/whyUs'
+import { Metadata } from 'next'
+const arabicMetadata: Metadata = {
+  title: 'أكاديمية أثر',
+  keywords: [
+    'تعلم',
+    'تطوير الويب',
+    'برمجة',
+    'تصميم',
+    'واجهات المستخدم',
+    'تطبيقات الجوال',
+    'تسويق الكتروني',
+    'تصميم جرافيك',
+  ],
+  description:
+    'اكاديمية أثر هي مكانك لتعلم مهارات جديدة وتطوير مهاراتك الحالية في مجالات متعددة مثل تطوير الويب، برمجة التطبيقات، تصميم واجهات المستخدم، تطبيقات الجوال، وتسويق الكتروني.',
+  alternates: {
+    canonical: 'https://athr-ksa.com/ar/learnings',
+    languages: {
+      en: 'https://athr-ksa.com/en/learnings',
+      'ar-SA': 'https://athr-ksa.com/ar/learnings',
+    },
+  },
+}
 
+const englishMetadata: Metadata = {
+  title: 'ATHR Academy',
+  keywords: [
+    'learn',
+    'web development',
+    'programming',
+    'design',
+    'UI/UX',
+    'mobile apps',
+    'digital marketing',
+    'graphic design',
+  ],
+  description:
+    'ATHR Academy is your place to learn new skills and develop your current skills in various fields such as web development, app development, UI/UX design, mobile apps, and digital marketing.',
+  alternates: {
+    canonical: 'https://athr-ksa.com/en/learnings',
+    languages: {
+      en: 'https://athr-ksa.com/en/learnings',
+      'ar-SA': 'https://athr-ksa.com/ar/learnings',
+    },
+  },
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { locale: string }
+}): Promise<Metadata> {
+  return params.locale === 'ar' ? arabicMetadata : englishMetadata
+}
 export default async function Page() {
   const getCourses = async () => {
-    const res = await fetch('https://new.athr-ksa.com/api/all-courses')
+    const res = await fetch('https://new.athr-ksa.com/api/all-courses/2')
     const data = await res.json()
     return data.data
   }
@@ -21,7 +74,8 @@ export default async function Page() {
       <Statistics />
       <WhyUs />
       <FeaturedCourses courses={courses} />
-      <FAQsSection name="OS.FAQs" />
+      <ReviewsSection />
+      <FAQsSection name="learnings.FAQs" />
       <Contact />
       <Newsletter />
     </div>
