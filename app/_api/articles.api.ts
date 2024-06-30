c6d3c2ac46fd111d317c4e5ccdcff53a5ced9bb1
MohamedRamdan1 <medo44448888@gmail.com>
2024-06-30 21:46:12 +0300

    refactor: Add API functions for fetching articles and categories

---
+++ app/_api/articles.api.ts
@@ -0,0 +1,36 @@
+const url = process.env.API_BASE_URL;
+
+export async function getUserIp() {
+  const res = await fetch("https://api.ipify.org?format=json");
+  const data = await res.json();
+  return data.ip;
+}
+
+const userIp = await getUserIp();
+
+async function getArticles(endpoint: string) {
+  const formData = new FormData();
+  formData.append("ip_address", userIp);
+  const res = await fetch(`${url}/${endpoint}`, {
+    method: "POST",
+    body: formData,
+  });
+  const data = await res.json();
+  return data.data;
+}
+
+export async function getArabicArticles() {
+  return await getArticles("all-artical-arabic");
+}
+
+export async function getEnglishArticles() {
+  return await getArticles("all-artical-english");
+}
+
+export async function getArticlesCategories() {
+  const res = await fetch(`${url}/all-category-english`, {
+    method: "GET",
+  });
+  const data = await res.json();
+  return data.data;
+}
